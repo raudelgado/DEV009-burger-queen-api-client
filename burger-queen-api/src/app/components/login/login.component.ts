474c550757e3f7a6f5473fff5b907a3f162bab10
Diana03m <dianamorales100599@gmail.com>
2023-10-17 09:25:35 -0300

    Agregar form con FormModule prueba

--- burger-queen-api/src/app/components/login/login.component.ts
+++ burger-queen-api/src/app/components/login/login.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  user = {
+    email: '',
+    password: ''
+  };
 
+  login() {
+    // Aquí puedes implementar la lógica de inicio de sesión
+    if (this.user.email === 'user@example.com' && this.user.password === 'password') {
+      console.log('Inicio de sesión exitoso');
+    } else {
+      console.log('Credenciales incorrectas');
+    }
+  }
+  
 }
