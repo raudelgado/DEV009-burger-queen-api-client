6e5fbca84b27bcf84c7902d832ad61b6a2c7f752
Diana03m <dianamorales100599@gmail.com>
2023-10-30 11:49:26 -0300

    Agregamos errores en login en caso de tener credenciales incorrectas, y no dejar hacer login si los campos estan en blanco

--- burger-queen-api/src/app/components/login/login.component.ts
+++ burger-queen-api/src/app/components/login/login.component.ts
@@ -10,37 +10,41 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   user = {
-     email: '',
-     password: ''
+    email: '',
+    password: ''
   };
   errorMessage = ''; // Declaración de la variable 'errorMessage'
- 
-  constructor (private loginService: LoginService, private router: Router) {}
- 
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
   login() {
-     const email = this.user.email; 
-     const password = this.user.password; 
-    
-     // Verificar si el correo electrónico y la contraseña están vacíos
-     if (!email || !password) {
-        console.error('El correo electrónico y la contraseña son obligatorios.');
-        return;
-     }
-    
-     this.loginService.login(email, password).subscribe(
-        (response: LoginAuth) => {
-          console.log('Respuesta de inicio de sesión:', response)
-          const accessToken = response.accessToken;
-          localStorage.setItem('accessToken', accessToken);
-    
-          this.router.navigate(['/New Order']);
-        },
-        (error) => {
-          console.error('Error de inicio de sesión', error);
-    
-          // Mostrar un mensaje de error descriptivo al usuario
-          this.errorMessage = 'El correo electrónico o la contraseña es incorrecta.';
-        }
-     );
+    const email = this.user.email;
+    const password = this.user.password;
+
+    // Verificar si el correo electrónico y la contraseña están vacíos
+    if (!email || !password) {
+      console.error('El correo electrónico y la contraseña son obligatorios.');
+      return;
     }
+
+    this.loginService.login(email, password).subscribe(
+      (response: LoginAuth) => {
+        console.log('Respuesta de inicio de sesión:', response);
+        const accessToken = response.accessToken;
+        localStorage.setItem('accessToken', accessToken);
+
+        this.router.navigate(['/New Order']);
+      },
+      (error) => {
+        console.error('Error de inicio de sesión', error);
+
+        // Mostrar un mensaje de error descriptivo al usuario
+        if (error.status === 401) {
+          this.errorMessage = 'Correo electrónico o contraseña incorrectos.';
+        } else {
+          this.errorMessage = 'Error en el inicio de sesión. Credenciales Incorrectas. Por favor, inténtalo de nuevo.';
+        }
+      }
+    );
   }
+}
